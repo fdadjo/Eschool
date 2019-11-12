17b882b720414d0015f1efa026ff9ff58f2fedac
Frantz DADJO <fdadjo@software-zone-pro.com>
2019-11-12 20:35:53 +0100

    First docker test

--- Eschool-services/Docker/run.sh
+++ Eschool-services/Docker/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "nameserver 213.186.33.99" >> /etc/resolv.conf
+cat /etc/resolv.conf
+/bin/sleep 15
 
-./*.war  --spring.profiles.active=prod,swagger
+
+java -jar *.jar  --spring.profiles.active=prod
